c90e80f8764da815a79ebade8d4ccf2d8c223541
Scramos <55661905+Scramos@users.noreply.github.com>
2019-09-22 16:40:15 -0500

    Create compn.v

---
+++ módulos/comparador/compn.v
@@ -0,0 +1,27 @@
+module compn#(
+    parameter inputsize = 4
+)( data_in, clk, enable, ab_out,done_compn);
+
+input enable;
+input [7:0] data_in;
+reg [inputsize - 1:0] A;
+reg [inputsize - 1:0] B;
+output reg [3:0]ab_out;
+output reg done_compn;
+
+always @ (posedge enable ) begin
+ A <= data_in[7:4];
+ B <= data_in[3:0];
+        if (A > B) begin
+        ab_out <= 1;
+        end else begin
+        ab_out <= 0;
+        end
+done_compn <= 1'b1;
+end
+always @(negedge enable) begin
+done_compn <= 1'b0;  
+end
+
+
+endmodule
