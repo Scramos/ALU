504197b0856bd774084e5b954f922e2100bfbf2e
Scramos <scramosi@unal.edu.co>
2019-09-27 17:40:08 -0500

    Update operador.v

--- módulos segunda entrega/operador.v
+++ módulos segunda entrega/operador.v
@@ -1,13 +1,13 @@
-module (instr,dato_A,dato_B, dato_out)
+module (instr,dato_A,dato_B,dato_out,clk,dato_mux)
 
 input [7:0] instr;
 reg [3:0] A;
 reg [3:0] B;
 output [3:0] dato_out;
-always @(*) begin
-if 
+always @(clk) begin
+
 	case (instr [7:5])
-	3'h0 :  begin
+	3'h0 : begin
 		A <= instr[3:0];
  		B <= instr[3:0]; 
 		dato_out <= A+B;
@@ -43,13 +43,30 @@ if
 		dato_out <=0;
 
 		end
-	3'h5 : 3 begin
+	3'h5 :  begin
+		data_out <= instr[3:0];
 
 		end
-	
+
+	3'h7 :  begin 
+		data_out <=instr[3:0];
+
+		end
+	endcase
 
 
 
 end
+always @(*) begin
 
+	case (instr[3:0]) 
+	3'h0: data_mux <= data_out; 
+	3'h1: data_mux <= data_out;
+	3'h2: data_mux <= data_out;
+	3'h3: data_mux <= data_out;
+	3'h4: data_mux <= data_out;
+	3'h5: data_mux <= data_out;
+	//3'h6: data_mux <= data_out;
+	3'h7: data_mux <= data_out;
+	endcase
 endmodule 
