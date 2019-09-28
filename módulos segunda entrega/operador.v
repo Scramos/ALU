c0c1c68f15e7819bfff084af202831bdd63adb5f
Scramos <scramosi@unal.edu.co>
2019-09-28 14:57:43 -0500

    Update operador.v

--- módulos segunda entrega/operador.v
+++ módulos segunda entrega/operador.v
@@ -1,9 +1,16 @@
-module (instr,dato_A,dato_B,dato_outsum,clk,dato_mux)
+module (instr,dato_A,dato_B,dato_outsum,dato_outcomp,dato_outsl,dato_outsr,dato_outcmi,dato_outcmm, dato_outlo,clk,dato_mux)
 
 input [7:0] instr;
 reg [3:0] A;
 reg [3:0] B;
-output [3:0] dato_outsum;
+reg [3:0] dato_outsum;
+reg [3:0] dato_outcomp;
+reg [3:0] dato_outsl;
+reg [3:0] dato_outsr;
+reg [3:0] dato_outscmi;
+reg [3:0] dato_outcmm;
+reg [3:0] dato_outlo;
+output [3:0] dato_mux;
 always @(clk) begin
 
 	case (instr [7:5])
@@ -15,22 +22,22 @@ always @(clk) begin
 		end 
 	3'h1:  begin
 		A <= instr[3:0];
-		dato_out <= ~A;
+		dato_outcomp <= ~A;
 		end
 	3'h2 : begin
 		A <= instr[3:0];
-		dato_out <= {A[2:0], 1'b0};
+		dato_outsl <= {A[2:0], 1'b0};
 		end
 	3'h3 :  begin
 		A <= instr[3:0];
-		dato_out <= {1'b0; A[3:1]};
+		dato_outsr <= {1'b0; A[3:1]};
 		end
 	3'h4 :  begin
 		A <= instr[3:0];
  		B <= instr[3:0];
         	if (A == B) begin
         	
- 		dato_out <= 1;
+ 		dato_outcmi <= 1;
         	end else begin
         	dato_out <= 0;
 		end
@@ -38,7 +45,7 @@ always @(clk) begin
 		A <= instr[3:0];
 		B <= instr[3:0];
 		if (A > B) begin
-		dato_out <=1;
+		dato_outcmm <=1;
 		end else begin
 		dato_out <=0;
 
@@ -49,7 +56,7 @@ always @(clk) begin
 		end
 
 	3'h7 :  begin 
-		data_out <=instr[3:0];
+		data_outlo <=instr[3:0];
 
 		end
 	endcase
@@ -57,16 +64,16 @@ always @(clk) begin
 
 
 end
-always @(*) begin
+always @(clk) begin
 
 	case (instr[3:0]) 
-	3'h0: data_mux <= data_out; 
-	3'h1: data_mux <= data_out;
-	3'h2: data_mux <= data_out;
-	3'h3: data_mux <= data_out;
-	3'h4: data_mux <= data_out;
-	3'h5: data_mux <= data_out;
+	3'h0: data_mux <= data_outsum; 
+	3'h1: data_mux <= data_outcomp;
+	3'h2: data_mux <= data_outsl;
+	3'h3: data_mux <= data_outsr;
+	3'h4: data_mux <= data_outcmi;
+	3'h5: data_mux <= data_outcmm;
 	//3'h6: data_mux <= data_out;
-	3'h7: data_mux <= data_out;
+	3'h7: data_mux <= data_outlo;
 	endcase
 endmodule 
