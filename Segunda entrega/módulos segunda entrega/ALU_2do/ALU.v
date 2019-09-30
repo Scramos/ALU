2124a3a8267837c8b67b40ca1cde300f11f1c7ab
Juan David Carrillo <jdcarrillor@unal.edu.co>
2019-09-30 05:53:43 -0500

    EntregaFinal

--- Segunda entrega/módulos segunda entrega/ALU_2do/ALU.v
+++ Segunda entrega/módulos segunda entrega/ALU_2do/ALU.v
@@ -1,18 +1,22 @@
 module ALU #( parameter  nbits = 4 , parameter Taminstr =  8,  parameter fin = 100, fout = 50 )
-( leds,instrucciones , clk ,  reset ,);
-input clk;
-input reset;
+( leds,instrucciones , clk ,  reset , active );
+input clk ;
+input reset ;
+input active ;
 input [Taminstr-1 :0] instrucciones;
 output [nbits-1 :0 ] leds;
 wire [nbits-1 :0 ] datoA ;
 wire [nbits-1 :0 ] datoB ;
  wire [nbits-1 : 0] resultado;
+ wire doneOp;
+ wire doneBr;
  wire rd; 
+ wire init_other; 
  reg clko = 1;
 
     reg [fout-1:0] count = 0;
  
- always @ (posedge clki) begin
+ always @ (posedge clk) begin
     
             count = count + 1;
             if (count == fin/fout) begin
@@ -22,9 +26,11 @@ wire [nbits-1 :0 ] datoB ;
     end
 
 Operaciones operaciones(.clk(clko),.instr(instrucciones), .A(datoA), .B(datoB), 
-.dato_mux(resultado) ,.data_outlo(leds), .rd(rd));
+.dato_mux(resultado) ,.data_outlo(leds), .rd(rd),.init(init_other),.done(doneOp));
 Banco_reg registros ( .clk(clk), .reset(reset), .instr(instrucciones), .data_result(resultado),
- .data_A(datoA),.data_B(datoB), .rd(rd));
+ .data_A(datoA),.data_B(datoB), .rd(rd) , .init(init_other),.done(doneBr));
+ Controlador control(.read_Instr(active), .init_Memory(init_other), .doneCalcule(doneOp), 
+ .initrd(rd),.doneresult(doneBr));
 
 
-endmodule // ALU 
+endmodule  // ALU 
--- Segunda entrega/módulos segunda entrega/ALU_2do/Banco_reg.v
+++ Segunda entrega/módulos segunda entrega/ALU_2do/Banco_reg.v
@@ -1,25 +1,32 @@
 module Banco_reg #(parameter Tamdata = 4, Tamrow = 2)
-(clk,reset, instr, data_result, data_A , data_B,rd);
+(clk,reset, instr, data_result, data_A , data_B,rd , done, init);
 input clk;
 input reset;
 input [7:0] instr;
 input [3:0] data_result;
 input rd;
-reg [Tamdata-1:0] memory [Tamrow-1:0] = {0000, 0000};
+input init;
+reg [Tamdata-1:0] memory [Tamrow-1:0] ;
 output reg [3:0] data_A;
 output reg [3:0] data_B;
+output reg done ;
+
 
 reg posicion ;
 reg pos_A;
 reg pos_B;
+initial begin
+    memory [0] = 4'b0000;
+    memory [1] = 4'b0000;
+end
 
-always @(posedge clk) begin  // decodificacion de la instruccion y envia el dato
-    case (instr[7:5])
+always @(posedge clk) begin
+if (init == 1) begin
+      case (instr[7:5])      // decodificacion de la instruccion y envia el dato
         3'h0 : begin
             posicion <= instr[4];
             pos_A <= instr[3];
-            pos_B <= instr[2];
-            
+            pos_B <= instr[2];  
         end
         3'h1 : begin
             posicion <= instr[4];
@@ -41,12 +48,10 @@ always @(posedge clk) begin  // decodificacion de la instruccion y envia el dato
         3'h5 : begin
             posicion <= instr[4];
             pos_A <= instr[3];
-            pos_B <= instr[2];
-            
+            pos_B <= instr[2];  
         end
         3'h6 : begin
-            posicion <= instr[4];
-                      
+            posicion <= instr[4];            
         end
         3'h7 : begin
             posicion <= instr[4];
@@ -57,17 +62,18 @@ always @(posedge clk) begin  // decodificacion de la instruccion y envia el dato
      data_A <= memory[pos_A];
     data_B <= memory[pos_B];
 end
-/*always @(posedge clk ) begin // Envio en memoria 
-    memory[posicion] <= data_result;
 end
-*/
+
 always @(posedge clk ) begin
+done <= 1'b0;
 if (reset == 1) begin
      memory[0] <= 4'b0000;
      memory[1] <= 4'b0000;
 end 
 if (rd == 1) begin
-   memory[posicion] <= data_result; 
+   memory[posicion] <= data_result;
+   done <= 1'b1;
+
 end // Escribe resultado    
 end
 
